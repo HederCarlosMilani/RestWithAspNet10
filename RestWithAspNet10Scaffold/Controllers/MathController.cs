bfeaa4aaea67e84779ad3b0daaf4a007273fc451
Heder Carlos <hedermilani@hotmail.com>
2025-12-13 13:44:17 -0300

    Implement math to sum and sub path values

---
+++ RestWithAspNet10Scaffold/Controllers/MathController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithAspNet10Scaffold.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MathController : ControllerBase
+    {
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult GetSum(string firstNumber, string secondNumber) 
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+
+                return Ok(sum);
+            }
+            return BadRequest("Invalid input");
+        }
+
+		[HttpGet("sub/{firstNumber}/{secondNumber}")]
+		public IActionResult GetSub(string firstNumber, string secondNumber)
+		{
+			if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+			{
+				decimal subValue = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+				return Ok(subValue);
+			}
+			return BadRequest("Invalid Input Values");
+		}
+
+		private decimal ConvertToDecimal(string strNumber)
+        {
+            decimal decimalValue;
+
+            if(decimal.TryParse(
+                strNumber,
+				System.Globalization.NumberStyles.Any,
+				System.Globalization.NumberFormatInfo.InvariantInfo,
+                out decimalValue
+                ))
+            {
+                return decimalValue;
+            }
+
+            return 0;
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            decimal decimalValue;
+            bool isNumeric = decimal.TryParse(
+                strNumber, 
+                System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out decimalValue
+                );
+
+            return isNumeric;
+        }
+    }
+}
