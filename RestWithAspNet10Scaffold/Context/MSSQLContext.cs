04eb36f7f363615bd1a0c41cf23e72056f785a94
Heder Carlos <hedermilani@hotmail.com>
2025-12-14 18:29:45 -0300

    Create configurationto connect to database ms sqlserver

---
+++ RestWithAspNet10Scaffold/Context/MSSQLContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestWithAspNet10Scaffold.Model;
+
+namespace RestWithAspNet10Scaffold.Context;
+
+public class MSSQLContext : DbContext
+{
+    public MSSQLContext(DbContextOptions<MSSQLContext> options) : base(options) { }
+    
+    public DbSet<Person> Persons { get; set; }
+}
