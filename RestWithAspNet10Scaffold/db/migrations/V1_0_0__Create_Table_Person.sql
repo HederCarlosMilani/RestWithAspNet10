df0e8cea5f1e76341b29661a8dcd7f11e4aca023
Heder Carlos <hedermilani@hotmail.com>
2025-12-16 18:21:11 -0300

    Implement migrations to database

---
+++ RestWithAspNet10Scaffold/db/migrations/V1_0_0__Create_Table_Person.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE dbo.person (
+    id BIGINT PRIMARY KEY IDENTITY(1,1),
+    first_name VARCHAR(80) NOT NULL,
+    last_name VARCHAR(80) NOT NULL,
+    address VARCHAR(100) NOT NULL,
+    gender VARCHAR(6) NOT NULL
+);
