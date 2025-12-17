0e93217349dcd52eea53527259ded2a78b5cd3fc
Heder Carlos <hedermilani@hotmail.com>
2025-12-17 16:59:36 -0300

    Create DTOs and converte PersonDto

---
+++ RestWithAspNet10Scaffold/Data/Dto/BookDto.cs
@@ -0,0 +1,10 @@
+﻿namespace RestWithAspNet10Scaffold.Data.Dto;
+
+public class BookDto
+{
+    public  long Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public decimal Price { get; set; }
+    public DateTime LaunchDate {get; set;}
+}
