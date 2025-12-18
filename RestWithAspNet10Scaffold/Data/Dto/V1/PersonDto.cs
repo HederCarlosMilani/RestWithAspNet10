60aa09b01632d48fd7d40d177469fbf3b97baa0f
Heder Carlos <hedermilani@hotmail.com>
2025-12-18 15:36:53 -0300

    Implement Serialization string and ignore with conditions

--- RestWithAspNet10Scaffold/Data/Dto/V1/PersonDto.cs
+++ RestWithAspNet10Scaffold/Data/Dto/V1/PersonDto.cs
@@ -1,16 +1,12 @@
 ﻿using System.Text.Json.Serialization;
+using RestWithAspNet10Scaffold.JsonSerializers;
 
 namespace RestWithAspNet10Scaffold.Data.Dto.V1;
 
 public class PersonDto
 {
-    [JsonPropertyName("code")]
     public long Id { get; set; }
-    
-    [JsonPropertyName("first_name")]
     public string FirstName { get; set; }
-    
-    [JsonPropertyName("last_name")]
     public string LastName { get; set; }
     public string Address { get; set; }
     public string Gender {get; set;}
--- RestWithAspNet10Scaffold/Data/Dto/V2/PersonDto.cs
+++ RestWithAspNet10Scaffold/Data/Dto/V2/PersonDto.cs
@@ -5,10 +5,20 @@ namespace RestWithAspNet10Scaffold.Data.Dto.V2;
 
 public class PersonDto
 {
+    [JsonPropertyName("code")]
     public long Id { get; set; }
+    
+    [JsonPropertyName("first_name")]
     public string FirstName { get; set; }
+    
+    [JsonPropertyName("last_name")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
     public string LastName { get; set; }
+    
+    [JsonIgnore]
     public string Address { get; set; }
+    
+    [JsonConverter(typeof(GenderSerializer))]
     public string Gender {get; set;}
     
     [JsonConverter(typeof(DateSerializer))]
