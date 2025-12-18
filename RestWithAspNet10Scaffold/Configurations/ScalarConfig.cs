e09fe878d9843207b13b6e7319b2c6e66b01c8ad
Heder Carlos <hedermilani@hotmail.com>
2025-12-18 17:34:22 -0300

    Implement scalar config and use

---
+++ RestWithAspNet10Scaffold/Configurations/ScalarConfig.cs
@@ -0,0 +1,22 @@
+﻿using Scalar.AspNetCore;
+
+namespace RestWithAspNet10Scaffold.Configurations;
+
+public static class ScalarConfig
+{
+    private static readonly string AppName = "RestWithAspNet10Scaffold";
+    private static readonly string AppDescription =
+        "A simple API built with ASP.NET Core 10 demonstrating RESTful principles and best practices.";
+
+    public static WebApplication UseScalarSpecification(this WebApplication app)
+    {
+        app.MapScalarApiReference("/scalar", options =>
+        {
+            options
+                .WithTitle(AppName)
+                .WithOpenApiRoutePattern("/swagger/V1.0.0/swagger.json");
+        });
+        
+        return app;
+    }
+}
--- RestWithAspNet10Scaffold/Program.cs
+++ RestWithAspNet10Scaffold/Program.cs
@@ -37,5 +37,6 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.UseSwaggerSpecification();
+app.UseScalarSpecification();
 
 app.Run();
