67a0511c41e65f04ff1b3c0ec5e108d63438e0e3
Heder Carlos <hedermilani@hotmail.com>
2025-12-19 15:56:14 -0300

    Implement integration test to swagger json response OK

---
+++ RestWithAspNet10Scafold.Tests/IntegrationTests/SwaggerIntegrationTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using RestWithAspNet10Scafold.Tests.IntegrationTests.Tools;
+
+namespace RestWithAspNet10Scafold.Tests.IntegrationTests;
+
+public class SwaggerIntegrationTests : IClassFixture<SqlServerFixture>
+{
+    private readonly HttpClient _httpClient;
+    
+    public SwaggerIntegrationTests(SqlServerFixture sqlServerFixture)
+    {
+        var factory = new CustomWebApplicationFactory<Program>(sqlServerFixture.ConnectionString);
+        _httpClient = factory.CreateClient(
+            new WebApplicationFactoryClientOptions
+            {
+                BaseAddress = new Uri("http://localhost")
+            }
+            );
+    }
+
+    [Fact]
+    public async Task Get_SwaggerJson_ShouldReturnOk()
+    {
+        // Arrange
+        var requestUrl = "/swagger/V1.0.0/swagger.json";
+        
+        // Act
+        var response = await _httpClient.GetAsync(requestUrl);
+        
+        // Assert
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        content.Should().NotBeNullOrEmpty();
+        content.Should().Contain("person/{id}");
+        content.Should().Contain("book/{id}");
+    }
+}
