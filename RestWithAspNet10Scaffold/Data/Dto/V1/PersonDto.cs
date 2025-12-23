3f39651166b4d4a562e0fc715913a9e1e8cb907b
Heder Carlos <hedermilani@hotmail.com>
2025-12-23 20:30:07 -0300

    Create person enricher

--- RestWithAspNet10Scaffold/Data/Dto/V1/PersonDto.cs
+++ RestWithAspNet10Scaffold/Data/Dto/V1/PersonDto.cs
@@ -1,9 +1,11 @@
 ﻿using System.Text.Json.Serialization;
+using RestWithAspNet10Scaffold.Hypermedia;
+using RestWithAspNet10Scaffold.Hypermedia.Abstract;
 using RestWithAspNet10Scaffold.JsonSerializers;
 
 namespace RestWithAspNet10Scaffold.Data.Dto.V1;
 
-public class PersonDto
+public class PersonDto : ISupportsHypermidea
 {
     public long Id { get; set; }
     public string FirstName { get; set; }
@@ -11,4 +13,5 @@ public class PersonDto
     public string Address { get; set; }
     public string Gender {get; set;}
     public bool Enabled { get; set; }
+    public List<HypermideaLink> Links { get; set; } = [];
 }
---
+++ RestWithAspNet10Scaffold/Hypermedia/Enricher/PersonEnricher.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestWithAspNet10Scaffold.Data.Dto.V1;
+using RestWithAspNet10Scaffold.Hypermedia.Constants;
+
+namespace RestWithAspNet10Scaffold.Hypermedia.Enricher;
+
+public class PersonEnricher : ContentResponseEnricher<PersonDto>
+{
+    protected override Task EnrichModel(PersonDto content, IUrlHelper urlHelper)
+    {
+        var request = urlHelper.ActionContext.HttpContext.Request;
+        var baseUrl = $"{request.Scheme}://{request.Host.ToUriComponent()}/person";
+        content.Links.AddRange(GenerateLinks(content.Id, baseUrl));
+        
+        return Task.CompletedTask;
+    }
+
+    private IEnumerable<HypermideaLink> GenerateLinks(long contentId, string baseUrl)
+    {
+        return (List<HypermideaLink>)
+        [
+            new()
+            {
+                Rel = RelationType.SELF,
+                Href = $"{baseUrl}/{contentId}",
+                Type = ResponseTypeFormat.DefaultGet,
+                Action = HttpActionVerb.GET
+            },
+
+            new()
+            {
+                Rel = RelationType.COLLECTION,
+                Href = $"{baseUrl}",
+                Type = ResponseTypeFormat.DefaultGet,
+                Action = HttpActionVerb.GET
+            },
+
+            new()
+            {
+                Rel = RelationType.CREATE,
+                Href = $"{baseUrl}",
+                Type = ResponseTypeFormat.DefaultPost,
+                Action = HttpActionVerb.POST
+            },
+
+            new()
+            {
+                Rel = RelationType.UPDATE,
+                Href = $"{baseUrl}",
+                Type = ResponseTypeFormat.DefaultPost,
+                Action = HttpActionVerb.PUT
+            },
+
+            new()
+            {
+                Rel = RelationType.ENABLE,
+                Href = $"{baseUrl}/enable/{contentId}",
+                Type = ResponseTypeFormat.DefaultPathch,
+                Action = HttpActionVerb.PATCH
+            },
+
+            new()
+            {
+                Rel = RelationType.DISABLE,
+                Href = $"{baseUrl}/disable/{contentId}",
+                Type = ResponseTypeFormat.DefaultPathch,
+                Action = HttpActionVerb.PATCH
+            },
+
+            new()
+            {
+                Rel = RelationType.DELETE,
+                Href = $"{baseUrl}/{contentId}",
+                Type = ResponseTypeFormat.DefaultDelete,
+                Action = HttpActionVerb.DELETE
+            }
+        ];
+    }
+}
