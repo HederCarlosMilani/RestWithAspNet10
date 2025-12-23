ea14a64cdb4b16865f8518cc16270f03f666cab2
Heder Carlos <hedermilani@hotmail.com>
2025-12-23 17:04:48 -0300

    Create url helper and hypermidea link to implement Hateoas

--- RestWithAspNet10Scaffold/Hypermedia/HypermideaLink.cs
+++ RestWithAspNet10Scaffold/Hypermedia/HypermideaLink.cs
@@ -2,5 +2,8 @@
 
 public class HypermideaLink
 {
-    
+    public string Rel { get; set; } = string.Empty;
+    public string Href { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+    public string Action { get; set; } = string.Empty;
 }
