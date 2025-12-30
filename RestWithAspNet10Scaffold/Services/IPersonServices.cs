9c3dde44246dadce4711b41cd07183e29b2c3dae
Heder Carlos <hedermilani@hotmail.com>
2025-12-30 17:32:04 -0300

    Implement paged search in service

--- RestWithAspNet10Scaffold/Services/IPersonServices.cs
+++ RestWithAspNet10Scaffold/Services/IPersonServices.cs
@@ -1,5 +1,6 @@
 ﻿using RestWithAspNet10Scaffold.Data.Dto;
 using RestWithAspNet10Scaffold.Data.Dto.V1;
+using RestWithAspNet10Scaffold.Hypermedia.Helpers;
 
 namespace RestWithAspNet10Scaffold.Services;
 
@@ -13,4 +14,5 @@ public interface IPersonServices
     PersonDto? Disable(long id);
     PersonDto? Enable(long id);
     List<PersonDto> FindByName(string? firstName, string? lastName);
+    PagedSearchDto<PersonDto> FindWithPagedSearch(string? name, string sortDirection, int pageSize, int page);
 }
--- RestWithAspNet10Scaffold/Services/Impl/PersonService.cs
+++ RestWithAspNet10Scaffold/Services/Impl/PersonService.cs
@@ -2,6 +2,7 @@
 using RestWithAspNet10Scaffold.Data.Convert.Impl;
 using RestWithAspNet10Scaffold.Data.Dto;
 using RestWithAspNet10Scaffold.Data.Dto.V1;
+using RestWithAspNet10Scaffold.Hypermedia.Helpers;
 using RestWithAspNet10Scaffold.Models;
 using RestWithAspNet10Scaffold.Repositories;
 
@@ -63,5 +64,41 @@ public class PersonService : IPersonServices
     {
         var persons = _personRepository.FindByName(firstName, lastName);
         return _personConverter.ParserList(persons);
+    }
+
+    public PagedSearchDto<PersonDto> FindWithPagedSearch(string? name, string sortDirection, int pageSize, int page)
+    {
+        var (query, countQuery, sort, size, offset) = BuildQueries(name, sortDirection, pageSize, page);
+        
+        var persons = _personRepository.FindWithPagedSearch(query);
+        var totalResults = _personRepository.GetCount(countQuery);
+
+        return new PagedSearchDto<PersonDto>
+        {
+            CurrentPage = page,
+            List = persons.Adapt<List<PersonDto>>(),
+            PageSize = size,
+            SortDirection = sort,
+            TotalResults = totalResults
+        };
+    }
+
+    private (string query, string countQuery, string sort, int size, int offset) BuildQueries(string? name, string sortDirection, int pageSize, int page)
+    {
+        page = Math.Max(1, page);
+        
+        var offset = (page - 1) * pageSize;
+        var size = pageSize < 1 ? 1 : pageSize;
+        
+        var sort = (!string.IsNullOrWhiteSpace(sortDirection) && sortDirection.Equals("desc", StringComparison.OrdinalIgnoreCase)) ? "desc" : "asc";
+        
+        var baseQuery = $@"FROM Person p WHERE 1=1 ";
+        
+        if (!string.IsNullOrWhiteSpace(name)) baseQuery += $" AND (p.FirstName LIKE '%{name}%') ";
+        
+        var countQuery = $"SELECT COUNT(*) {baseQuery}";
+        var query = $@"SELECT * {baseQuery} ORDER BY p.FirstName {sort} OFFSET {offset} ROWS FETCH NEXT {size} ROWS ONLY";
+        
+        return (query, countQuery, sort, size, offset);
     }
 }
