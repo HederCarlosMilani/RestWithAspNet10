435a23ec5de22fdf34bd08300cb41530edfe1c6a
Heder Carlos <hedermilani@hotmail.com>
2026-01-02 11:07:40 -0300

    Implement Export to Csv and Excel

--- RestWithAspNet10Scaffold/Files/Exporters/Impl/CsvFileExporter.cs
+++ RestWithAspNet10Scaffold/Files/Exporters/Impl/CsvFileExporter.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Globalization;
+using System.Text;
+using CsvHelper;
+using CsvHelper.Configuration;
+using Microsoft.AspNetCore.Mvc;
 using RestWithAspNet10Scaffold.Data.Dto.V1;
 using RestWithAspNet10Scaffold.Files.Exporters.Contract;
+using RestWithAspNet10Scaffold.Files.Exporters.Factory;
 
 namespace RestWithAspNet10Scaffold.Files.Exporters.Impl;
 
@@ -8,6 +13,25 @@ public class CsvFileExporter : IFileExporter
 {
     public FileContentResult ExportFile(List<PersonDto> persons)
     {
-        throw new NotImplementedException();
+        using var memoryStream = new MemoryStream();
+        using var streamWriter = new StreamWriter(memoryStream, Encoding.UTF8, leaveOpen: true);
+
+        using var csv = new CsvWriter(
+            streamWriter,
+            new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = true
+            }
+        );
+        
+        csv.WriteRecords(persons);
+        streamWriter.Flush();
+        
+        var fileBytes = memoryStream.ToArray();
+
+        return new FileContentResult(fileBytes, MediaTypes.ApplicationCsv)
+        {
+            FileDownloadName = $"Persons_exported_{DateTime.UtcNow:yyyyMMddHHmmss}.csv"
+        };
     }
 }
