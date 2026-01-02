97c08bca6059ee7e3c8b0e4c6d726b96c4a329a6
Heder Carlos <hedermilani@hotmail.com>
2026-01-02 19:33:40 -0300

    Implement Password hasher with sha256

---
+++ RestWithAspNet10Scaffold/Auth/Tools/Sha256PasswordHasher.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using RestWithAspNet10Scaffold.Auth.Contract;
+
+namespace RestWithAspNet10Scaffold.Auth.Tools;
+
+public class Sha256PasswordHasher : IPasswordHasher
+{
+    public string Hash(string password)
+    {
+        var bytes = Encoding.UTF8.GetBytes(password);
+        var hash = SHA256.HashData(bytes);
+        
+        var stringBuilder = new StringBuilder();
+        foreach (var item in hash) 
+            stringBuilder.Append(item.ToString("x2"));
+        
+        return stringBuilder.ToString();
+    }
+
+    public bool Verify(string password, string hashedPassword)
+    {
+        return (hashedPassword == Hash(password));
+    }
+}
