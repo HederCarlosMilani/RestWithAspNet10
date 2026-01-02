831496f6bff9a84878125594707194d206f78608
Heder Carlos <hedermilani@hotmail.com>
2026-01-02 19:42:18 -0300

    Implement dtos to consumer users and tokens

---
+++ RestWithAspNet10Scaffold/Data/Dto/V1/TokenDto.cs
@@ -0,0 +1,19 @@
+﻿namespace RestWithAspNet10Scaffold.Data.Dto.V1;
+
+public class TokenDto
+{
+    public TokenDto(bool authenticated, string created, string expiration, string accessToken, string refreshToken)
+    {
+        Authenticated = authenticated;
+        Created = created;
+        Expiration = expiration;
+        AccessToken = accessToken;
+        RefreshToken = refreshToken;
+    }
+
+    public bool Authenticated { get; set; }
+    public string Created { get; set; }
+    public string Expiration { get; set; }
+    public string AccessToken { get; set; }
+    public string RefreshToken { get; set; }
+}
---
+++ RestWithAspNet10Scaffold/Data/Dto/V1/UserDto.cs
@@ -0,0 +1,7 @@
+﻿namespace RestWithAspNet10Scaffold.Data.Dto.V1;
+
+public class UserDto
+{
+    string UserName { get; set; }
+    string Password { get; set; }
+}
