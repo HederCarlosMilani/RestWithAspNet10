70aadea072e8902d997c663e34ccef232da64df0
Heder Carlos <hedermilani@hotmail.com>
2026-01-03 17:04:03 -0300

    Implement generate token

---
+++ RestWithAspNet10Scaffold/Auth/Contract/ITokenGenerator.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+
+namespace RestWithAspNet10Scaffold.Auth.Contract;
+
+public interface ITokenGenerator
+{
+    string GenerateAccessToken(IEnumerable<Claim> claims);
+    string GenerateRefreshToken();
+    ClaimsPrincipal GetPrincipalFromExpiredToken(string token);
+}
---
+++ RestWithAspNet10Scaffold/Auth/Tools/TokenGenerator.cs
@@ -0,0 +1,59 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using RestWithAspNet10Scaffold.Auth.Configuration;
+using RestWithAspNet10Scaffold.Auth.Contract;
+
+namespace RestWithAspNet10Scaffold.Auth.Tools;
+
+public class TokenGenerator(TokenConfig configurations) : ITokenGenerator
+{
+    public string GenerateAccessToken(IEnumerable<Claim> claims)
+    {
+        var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configurations.Secret));
+        
+        var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+        var tokenOptions = new JwtSecurityToken
+        (
+            issuer: configurations.Issuer,
+            audience: configurations.Audience,
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(configurations.Minutes),
+            signingCredentials: signingCredentials
+        );
+        
+        return new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+    }
+
+    public string GenerateRefreshToken()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
+    {
+        var tokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false,
+            ValidateIssuer = false,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configurations.Secret)),
+            ValidateLifetime = false,
+            ValidIssuer = configurations.Issuer,
+            ValidAudience = configurations.Audience
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out var securityToken);
+
+        if (securityToken is not JwtSecurityToken jwtSecurityToken ||
+            !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
+        {
+            throw new SecurityTokenException("Invalid token");
+        }
+
+        return principal;
+    }
+}
