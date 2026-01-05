c1138a454b8547805ed091aa0133e502276a5291
Heder Carlos <hedermilani@hotmail.com>
2026-01-05 20:31:05 -0300

    Implement generate refresh token

--- RestWithAspNet10Scaffold/Auth/Tools/TokenGenerator.cs
+++ RestWithAspNet10Scaffold/Auth/Tools/TokenGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using Microsoft.IdentityModel.Tokens;
 using RestWithAspNet10Scaffold.Auth.Configuration;
@@ -29,7 +30,10 @@ public class TokenGenerator(TokenConfig configurations) : ITokenGenerator
 
     public string GenerateRefreshToken()
     {
-        throw new NotImplementedException();
+        var randomNumber = new byte[32];
+        using var rng = RandomNumberGenerator.Create();
+        rng.GetBytes(randomNumber);
+        return Convert.ToBase64String(randomNumber);
     }
 
     public ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
